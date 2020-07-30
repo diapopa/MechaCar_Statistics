99b072e6d4ea517eca0dfd06fbdb158ad9a6fe4c
Diana <63475737+diapopa@users.noreply.github.com>
2020-07-30 19:24:57 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,22 @@
+#read the mechacar data
+mecha_data <- read.csv("MechaCar_mpg.csv",stringsAsFactors = F,check.names = F)
+
+#create linear regression model
+mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_data)
+summary(mecha_lm)
+
+#Suspension Coil Summary
+mean(SuspensionTable$PSI)
+median(SuspensionTable$PSI)
+var(SuspensionTable$PSI)
+sd(SuspensionTable$PSI)
+
+#T-tests
+# Lot 1
+t.test(subset(SuspensionTable,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+# Lot 2
+t.test(subset(SuspensionTable,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+# Lot 3
+t.test(subset(SuspensionTable,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+# Across all Lots
+t.test(SuspensionTable$PSI,mu = 1500)
